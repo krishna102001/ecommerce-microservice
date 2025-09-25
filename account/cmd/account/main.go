3a29b93397cf2385ecf164e8d41dd63010cb6332
krishna102001 <krishnakantmaurya@icloud.com>
2025-09-25 21:14:37 +0530

    server initialization file

---
+++ account/cmd/account/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/krishna102001/ecommerce-microservice/account"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r account.Respository
+
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = account.NewPostgresRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	})
+	defer r.Close()
+
+	log.Println("Listening on port 8080...")
+
+	s := account.NewService(r)
+	log.Fatal(account.ListenGRPC(s, 8080))
+
+}
