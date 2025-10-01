f6610fb764eb397a3a386887a301fc434ac94222
krishna102001 <krishnakantmaurya@icloud.com>
2025-10-01 13:45:05 +0530

    order file structure

--- order/up.sql
+++ order/up.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS orders(
     account_id CHAR(27) NOT NULL,
     total_price MONEY NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS order_products(
+    order_id CHAR(27) REFERENCES orders(id) ON DELETE CASCADE,
+    product_id CHAR(27),
+    quantity INT NOT NULL,
+    PRIMARY KEY(product_id,order_id)
+);
