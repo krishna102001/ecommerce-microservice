2e1e1abd1790e51e647c201e7244dc60a72b8d3d
krishna102001 <krishnakantmaurya@icloud.com>
2025-10-06 16:23:48 +0530

    graphql done

--- graphql/account_resolver.go
+++ graphql/account_resolver.go
@@ -1,9 +1,47 @@
 package main
 
+import (
+	"context"
+	"log"
+	"strconv"
+	"time"
+)
+
 type accountResolver struct {
 	server *Server
 }
 
-// func (r *accountResolver) Orders(ctx context.Context, obj *Account) ([]*Order, error) {
+func (r *accountResolver) Orders(ctx context.Context, obj *Account) ([]*Order, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
 
-// }
+	orderList, err := r.server.orderClient.GetOrdersForAccount(ctx, obj.ID)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	var orders []*Order
+	for _, o := range orderList {
+		var products []*OrderedProduct
+		for _, p := range o.Products {
+			pQuantity, _ := strconv.Atoi(p.Quantity)
+			pPrice, _ := strconv.ParseFloat(p.Price, 2)
+			products = append(products, &OrderedProduct{
+				ID:          p.ID,
+				Name:        p.Name,
+				Description: p.Description,
+				Quantity:    pQuantity,
+				Price:       pPrice,
+			})
+		}
+		oTotalPrice, _ := strconv.ParseFloat(o.TotalPrice, 2)
+		orders = append(orders, &Order{
+			ID:         o.ID,
+			CreatedAt:  o.CreatedAt,
+			TotalPrice: oTotalPrice,
+			Products:   products,
+		})
+	}
+	return orders, nil
+}
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,17 +1,80 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/krishna102001/ecommerce-microservice/order"
+)
+
+var (
+	ErrInvalidParameter = errors.New("invalid parameter")
+)
+
 type mutationResolver struct {
 	server *Server
 }
 
-// func (r *mutationResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
+func (r *mutationResolver) CreateAccount(ctx context.Context, in AccountInput) (*Account, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
 
-// }
+	a, err := r.server.accountClient.PostAccount(ctx, in.Name)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &Account{
+		ID:   a.ID,
+		Name: a.Name,
+	}, nil
+}
 
-// func (r *mutationResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
+func (r *mutationResolver) CreateProduct(ctx context.Context, in ProductInput) (*Product, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	inPrice := strconv.FormatFloat(in.Price, 'f', 2, 2)
+	p, err := r.server.catalogClient.PostProduct(ctx, in.Name, in.Description, inPrice)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	pPrice, _ := strconv.ParseFloat(p.Price, 2)
+	return &Product{
+		ID:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       pPrice,
+	}, nil
+}
 
-// }
+func (r *mutationResolver) CreateOrder(ctx context.Context, in OrderInput) (*Order, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
 
-// func (r *mutationResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
-
-// }
+	var products []order.OrderedProduct
+	for _, p := range in.Products {
+		if p.Quantity <= 0 {
+			return nil, ErrInvalidParameter
+		}
+		pQuantity := strconv.Itoa(p.Quantity)
+		products = append(products, order.OrderedProduct{
+			ID:       p.ID,
+			Quantity: pQuantity,
+		})
+	}
+	o, err := r.server.orderClient.PostOrder(ctx, in.AccountID, products)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	oTotalPrice, _ := strconv.ParseFloat(o.TotalPrice, 2)
+	return &Order{
+		ID:         o.ID,
+		CreatedAt:  o.CreatedAt,
+		TotalPrice: oTotalPrice,
+	}, nil
+}
